7b28cf0035e030900f563cca3ea3b0b0caf59ae3
PdoubleU <piotrwitasik1988@gmail.com>
2020-04-19 12:55:22 +0200

    Updates

---
+++ precache-manifest.015717ee28ad64378aa12b124ce5de7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86b37abc11b0b4411d33010133d1d99",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8cf0845cb4119a0276ce",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b540d465eafd5a677771",
+    "url": "/my-app/static/js/2.773833a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.773833a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf0845cb4119a0276ce",
+    "url": "/my-app/static/js/main.3e633b3d.chunk.js"
+  },
+  {
+    "revision": "9a96bac3add083d02f4c",
+    "url": "/my-app/static/js/runtime-main.d6bf61a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
