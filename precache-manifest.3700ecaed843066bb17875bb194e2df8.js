4ff64a0c04762fda6d73acb4eb188480d9d73c3e
PdoubleU <piotrwitasik1988@gmail.com>
2021-03-29 19:30:09 +0200

    Updates

---
+++ precache-manifest.3700ecaed843066bb17875bb194e2df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150f8964e6785bedf5ad9049f1eec5b0",
+    "url": "/react_timer_exercise_04_2020/index.html"
+  },
+  {
+    "revision": "cbf9d2fccde51661d6c1",
+    "url": "/react_timer_exercise_04_2020/static/css/main.da5ff9e0.chunk.css"
+  },
+  {
+    "revision": "a8b2f36c914a722209cd",
+    "url": "/react_timer_exercise_04_2020/static/js/2.70acd7e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_timer_exercise_04_2020/static/js/2.70acd7e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf9d2fccde51661d6c1",
+    "url": "/react_timer_exercise_04_2020/static/js/main.754b22a0.chunk.js"
+  },
+  {
+    "revision": "5113cd265bfb6f0a0488",
+    "url": "/react_timer_exercise_04_2020/static/js/runtime-main.2b30c476.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PdoubleU/react_timer_exercise_04_2020/precache-manifest.582e9b1bd1599d5fd1228f0ee8bf67fc.js"
+  "/react_timer_exercise_04_2020/precache-manifest.3700ecaed843066bb17875bb194e2df8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PdoubleU/react_timer_exercise_04_2020/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_timer_exercise_04_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
